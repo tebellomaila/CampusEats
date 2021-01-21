771996a5027250f47c546f06e61c5fc6bfc95b4c
mnisi-coder <59182644+mnisi-coder@users.noreply.github.com>
2021-01-21 22:42:47 +0200

    Add files via upload

--- api/User/login.js
+++ api/User/login.js
@@ -2,9 +2,10 @@ var http = require("http");
 var express = require('express');
 var app = express();
 var router = express.Router(); // this one and var app = express(); work the same way 
-
+// require the bcrypt module
+///var bcrypt = require('bcrypt');
 var bodyParser = require('body-parser');
-
+var session = require('express-session')
 // this will bring the code for database connection
 var connection = require('../..//connection'); // this path goes out the current directory and goes to find connction  
 
@@ -53,20 +54,31 @@ app.get('/', function(request, response) {
 app.post('/login', function(request, response) {
     var email = request.body.email;
     var password = request.body.password;
-    if (username && password) {
+	var Roles = request.body.Roles;
+	console.log(email);
+	console.log(password);
+	console.log(Roles);
+	
+	
+    if (email && password) {
 // check if user exists
         connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
             if (results.length > 0) {
-                request.session.loggedin = true;
+				
+				response.send('Login Successful');
+                /*request.session.loggedin = true;
                 request.session.email = email;
-				request.session.role = results[0].Roles;
+				request.session.role = results[0].Roles;*/
 				
 				
-				if(typeof req.session.message == 'customer')
+				if(Roles == "customer")
 				{
-					response.redirect('/Customer');
+					//response.redirect('/Customer');
+					response.send('Customer PAge');
+					
 				}else{
-					response.redirect('/Admin');
+					//response.redirect('/Admin');
+					response.send('Admin PAge');
 				}
 				 
             } else {
@@ -76,6 +88,86 @@ app.post('/login', function(request, response) {
         });
     } else {
         response.send('Please enter Username and Password!');
+		
         response.end();
     }
+});
+
+app.post('/register', function(request, response) {
+	
+	
+	var email = request.body.Email;
+    var password = request.body.Password;
+	var name = request.body.Name;
+	var role = request.body.Roles;
+	
+	
+	
+	console.log(name);
+	console.log(email);
+	console.log(password);
+	console.log(role);
+	
+	
+	
+    if (email && password && name && role) {
+    // check if user exists
+        connection.query('SELECT * FROM users WHERE email = ?', [email], function(error, results, fields) {
+            if (results.length > 0)
+			{
+                response.send('User Already has an account');
+            }else{
+					var today = new Date();
+					var user={
+						"name":request.body.Name,
+						"email":request.body.Email,
+						"password":request.body.Password,
+						"Roles":request.body.Roles,
+						
+					}
+					
+					
+					
+					connection.query('INSERT INTO users SET ?',[user], function (error, results, fields) {
+					  if (error) {
+						/*res.json({
+							status:false,
+							message:'there are some error with query'
+							
+						})*/
+						response.send('there are some error with query');
+						
+					  }else{
+						  /*res.json({
+							status:true,
+							data:results,
+							message:'user registered sucessfully'
+							
+						})*/
+						response.send('user registered sucessfully');
+						
+					  }
+					});
+
+			}				
+	
+		
+ 
+		});
+		
+		
+	} else{
+	response.send('Please enter values');	
+	}
+	
+});
+
+
+
+app.delete('/deleteUser/:email', function (req, res) {
+   //console.log(req.body);
+   connection.query('DELETE FROM users where email=?', [req.params.email], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end('User has been deleted!');
+	});
 });
